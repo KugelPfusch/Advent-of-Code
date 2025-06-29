6c078d896c20169d0f0331cd7ec25da23d1afdac
Florian Adunka <adunkaf@edu.htl-villach.at>
2025-06-29 15:56:06 +0200

    finished part 1

--- 2024/Day_01/part_1.js
+++ 2024/Day_01/part_1.js
@@ -8,14 +8,33 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // = Copyright (c) NullDev = //
 // ========================= //
 
-const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).trim().split("\n");
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt")))
+    .trim()
+    .split("\n");
 
 const pStart = performance.now();
 
-//
-// YOUR CODE HERE
-//
-const result = "...";
+let left = [];
+let right = [];
+let res = 0;
+let diff = 0;
+
+INPUT.forEach((line, idx) => {
+    [left[idx], right[idx]] = line.split("   ");
+});
+
+left = left.sort((a, b) => a - b);
+right = right.sort((a, b) => a - b);
+
+left.forEach((element, idx) => {
+    if (left[idx] > right[idx]) {
+        res += left[idx] - right[idx];
+    } else {
+        res += right[idx] - left[idx];
+    }
+});
+
+const result = res;
 
 const pEnd = performance.now();
 
